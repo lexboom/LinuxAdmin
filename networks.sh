22958b979d2e93d9db6ca598dc9e487b466d788b
lexboom <47512002+lexboom@users.noreply.github.com>
2020-10-26 13:35:18 -0400

    Create networks.sh
    the script needs to be run on the the home directory, the df.log will be appended with the information from the commands  thanks to the tee -a command. The df.log can be found in the users home directory. I chose the commands I did because I thought they could be useful. If I find better commands that I would like to use in the future, this file will be simple enough to modify.

---
+++ networks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#network info script
+
+(arp; ifconfig; hostname; route; tcptrack) | tee -a df.log                             # arp displays and modifies entries in the Address Resolution Protocol cache
+                                                                                       # ifconfig is used for displaying current network configuration information
+                                                                                       # hostname A hostname command is used to view a computer's hostname and domain name (DNS) (Domain Name Service), and to display or set a computer's hostname or domain name
+                                                                                       # The route command distinguishes between routes to hosts and routes to networks by interpreting the network address of the Destination variable
+                                                                                       # tcptrack tcptrack displays the status of TCP connections that it sees on a given network interface
