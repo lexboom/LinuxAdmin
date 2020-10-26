ec0b7066660418994e4c97bde30c500e9684c962
lexboom <47512002+lexboom@users.noreply.github.com>
2020-10-26 13:55:02 -0400

    Update networks.sh

--- networks.sh
+++ networks.sh
@@ -8,3 +8,5 @@
                                                                                        # hostname A hostname command is used to view a computer's hostname and domain name (DNS) (Domain Name Service), and to display or set a computer's hostname or domain name
                                                                                        # The route command distinguishes between routes to hosts and routes to networks by interpreting the network address of the Destination variable
                                                                                        # tcptrack tcptrack displays the status of TCP connections that it sees on a given network interface
+
+                                                                                       # tee -a will append the output to the file df.log so that we can check the information whenever we like!
